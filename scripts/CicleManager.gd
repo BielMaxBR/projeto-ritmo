0b47ce31d914bd8c39e4c237e6323848cf961307
BielMaxBR <gbielmaximo@gmail.com>
2021-08-25 21:46:50 -0300

    sinais

--- scripts/CicleManager.gd
+++ scripts/CicleManager.gd
@@ -5,15 +5,28 @@ export(float) var bpm = 120
 var delay = bpm/60/4
 
 var batida = 0
-var meia = 0
 var quarta = 0
+
+signal quarta
+signal batida
 
 func _ready():
 	$timer.wait_time = delay
 	$timer.start()
+	
+	emit_signal("quarta", quarta)
+	emit_signal("batida", batida)
 
 func _quarta():
 	quarta += 1
-	if quarta == 4: quarta = 0
+	emit_signal("quarta", quarta)
+	
+	if quarta == 4: 
+		quarta = 0
+		batida += 1
+	emit_signal("batida", batida)
+	
+	if batida == 4: 
+		batida = 0
 
 	print(quarta)
