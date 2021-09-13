fa8af89105f0cb8fab6db9a788ecae13e3dabbc2
BielMaxBR <gbielmaximo@gmail.com>
2021-09-13 14:17:00 -0300

    coisas q fiz no fds

--- scripts/Player/Player.gd
+++ scripts/Player/Player.gd
@@ -1,9 +1,9 @@
 extends KinematicBody2D
 
 var vel = Vector2.ZERO
-export var speed = 120
-export var gravity = 550
-export var jump_force = 250
+export var speed = 90
+export var gravity = 450
+export var jump_force = 216
 
 func _ready():
 	pass
@@ -19,13 +19,14 @@ func input():
 	var left = Input.is_action_pressed('left')
 	var down = Input.is_action_pressed('down')
 
-	
+	if is_on_floor():
+		vel.y = 0
 	if jump and is_on_floor():
 		vel.y = -jump_force
 	if stopJump and not is_on_floor() and vel.y < 0:
-		vel.y = jump_force/6
+		vel.y /= 3
 	
-	vel.x = speed * (Input.get_action_strength("right") - Input.get_action_strength("left"))
+	vel.x = speed * (right - left)
 	
 func _physics_process(delta):
 	input()
