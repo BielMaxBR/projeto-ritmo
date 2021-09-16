e090ea4ecd479cebc819c07c7f9bf7e89aa3069b
BielMaxBR <gbielmaximo@gmail.com>
2021-09-16 17:02:31 -0300

    limao some

--- scripts/Deleter.gd
+++ scripts/Deleter.gd
@@ -1,10 +1,4 @@
 extends Node2D
 
-func _on_VisibilityNotifier2D_screen_exited():
-	get_parent().queue_free()
-	print("a")
-
-
 func _on_VisibilityNotifier2D_viewport_exited(viewport):
 	get_parent().queue_free()
-	print("a")
